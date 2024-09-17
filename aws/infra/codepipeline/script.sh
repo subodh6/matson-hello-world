b6ac85a3f467b87f9949c37e8959726307f399aa
subodh6 <46165221+subodh6@users.noreply.github.com>
2024-09-17 17:01:17 +0530

    Update script.sh

--- aws/infra/codepipeline/script.sh
+++ aws/infra/codepipeline/script.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
- 
 VERSION="0.2"
- 
 # Install phase
  
 echo "Skipping yum update..."
  
 pip install boto3
- 
 # Pre-build phase
  
 echo "version controller"
@@ -21,50 +18,36 @@ sudo yum install zip -y
 pip install requests
  
 pip install ruamel.yaml
- 
 # Build phase
  
 echo "Source artifact reference: $CODEBUILD_SOURCE_VERSION"
  
 echo "Build started"
- 
- 
-cp -r aws/infra/codepipeline/* Ohana-Springboot/
- 
- 
-cd Ohana-Springboot || exit
- 
- 
-ls
- 
- 
-#export CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact get-authorization-token --domain matson --domain-owner 891377353125 --region us-east-1 --query authorizationToken --output text)
- 
+
  
 echo "Integration of CodeArtifact and authorizing"
- 
+
  
 echo "Build Initiated"
  
 mvn clean install -s settings.xml
  
 echo "Build Completed"
- 
 # Post-build phase
  
 echo "Downloading Required files for codedeploy"
- 
+
  
 cp target/cas-scheduler-admin-1.0.2.war .
- 
+
  
 mv cas-scheduler-admin-1.0.2.war cas-scheduler.war
- 
+
  
 mkdir build_artifacts
  
 cp cas-scheduler.war buildspec-lab.yml appspec.yml application_start.sh build_artifacts/
- 
+
  
 echo "Build artifacts prepared:"
  
